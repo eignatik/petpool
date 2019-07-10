bd11c56f6d417d20344d060783337117a888e39d
lightway82 <lightway82@gmail.com>
2019-07-10 22:14:33 +0300

    Добавлен ломбок. Логер заменен на современный logback через slf4 Поправлен класс конфига базы с учетом ломбока.

--- src/main/java/com/petpool/db/DataBaseProperties.java
+++ src/main/java/com/petpool/db/DataBaseProperties.java
@@ -1,5 +1,8 @@
 package com.petpool.db;
 
+import lombok.Data;
+
+@Data
 public class DataBaseProperties {
     private String url;
     private String name;
@@ -9,68 +12,4 @@ public class DataBaseProperties {
     private String hibernateSessionContextClass;
     private boolean hibernateShowSql;
     private String hibernateDialect;
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getDriverClass() {
-        return driverClass;
-    }
-
-    public void setDriverClass(String driverClass) {
-        this.driverClass = driverClass;
-    }
-
-    public int getHibernatePoolSize() {
-        return hibernatePoolSize;
-    }
-
-    public void setHibernatePoolSize(int hibernatePoolSize) {
-        this.hibernatePoolSize = hibernatePoolSize;
-    }
-
-    public String getHibernateSessionContextClass() {
-        return hibernateSessionContextClass;
-    }
-
-    public void setHibernateSessionContextClass(String hibernateSessionContextClass) {
-        this.hibernateSessionContextClass = hibernateSessionContextClass;
-    }
-
-    public boolean isHibernateShowSql() {
-        return hibernateShowSql;
-    }
-
-    public void setHibernateShowSql(boolean hibernateShowSql) {
-        this.hibernateShowSql = hibernateShowSql;
-    }
-
-    public String getHibernateDialect() {
-        return hibernateDialect;
-    }
-
-    public void setHibernateDialect(String hibernateDialect) {
-        this.hibernateDialect = hibernateDialect;
-    }
 }
--- src/main/java/com/petpool/web/HealthController.java
+++ src/main/java/com/petpool/web/HealthController.java
@@ -1,9 +1,11 @@
 package com.petpool.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j//use log.error( ...
 @RestController
 public class HealthController {
 
