0584ab35d10593a3b27aa9238587112cda086017
lightway82 <lightway82@gmail.com>
2019-07-11 00:24:23 +0300

    Apply PR comments

--- src/main/java/com/petpool/web/HealthController.java
+++ src/main/java/com/petpool/web/HealthController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j//use log.error( ...
+@Slf4j
 @RestController
 public class HealthController {
 
