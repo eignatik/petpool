35d6e13f042fa2c753e5ea1436ca73fd53319b5b
lightway82 <lightway82@gmail.com>
2019-07-12 17:09:07 +0300

    Set target_java_verssion to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,8 +81,8 @@ dependencies {
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_12
-    targetCompatibility = JavaVersion.VERSION_12
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 
 }
 
