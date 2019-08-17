eb72671abbf2df7f57306f71fb2b823a16fab925
Ilya Bulavin <Sam47kon@gmail.com>
2019-08-17 19:05:06 +0300

    Parse user device info from request
    #31

---
+++ src/main/java/com/petpool/application/util/useragent/UserAgentParserStrategyImpl.java
@@ -0,0 +1,30 @@
+package com.petpool.application.util.useragent;
+
+import static nl.basjes.parse.useragent.UserAgent.AGENT_NAME;
+import static nl.basjes.parse.useragent.UserAgent.AGENT_VERSION;
+import static nl.basjes.parse.useragent.UserAgent.OPERATING_SYSTEM_NAME;
+import static nl.basjes.parse.useragent.UserAgent.OPERATING_SYSTEM_VERSION;
+
+import nl.basjes.parse.useragent.UserAgent;
+import nl.basjes.parse.useragent.UserAgentAnalyzer;
+
+public class UserAgentParserStrategyImpl implements UserAgentParserStrategy {
+
+  @Override
+  public UserAgentParserResult parse(String userAgent) {
+    UserAgentAnalyzer analyzer = UserAgentAnalyzer
+        .newBuilder()
+        .build();
+
+    UserAgent agent = analyzer.parse(userAgent);
+
+    String osName = agent.getValue(OPERATING_SYSTEM_NAME);
+    String osVersion = agent.getValue(OPERATING_SYSTEM_VERSION);
+    String browserName = agent.getValue(AGENT_NAME);
+    String browserVersion = agent.getValue(AGENT_VERSION);
+
+    OS os = new OS(osName, osVersion);
+    Browser browser = new Browser(browserName, browserVersion);
+    return new UserAgentParserResult(os, browser);
+  }
+}
