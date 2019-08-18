7e739300a236105e40e10ebd861fb011c911e5e3
Ilya Bulavin <Sam47kon@gmail.com>
2019-08-18 23:31:42 +0300

    Parse user device info from request // EDIT changed test cases
    #31

--- src/main/java/com/petpool/application/util/useragent/UserAgentParserStrategyImpl.java
+++ src/main/java/com/petpool/application/util/useragent/UserAgentParserStrategyImpl.java
@@ -4,7 +4,10 @@ import static nl.basjes.parse.useragent.UserAgent.AGENT_NAME;
 import static nl.basjes.parse.useragent.UserAgent.AGENT_VERSION;
 import static nl.basjes.parse.useragent.UserAgent.OPERATING_SYSTEM_NAME;
 import static nl.basjes.parse.useragent.UserAgent.OPERATING_SYSTEM_VERSION;
+import static nl.basjes.parse.useragent.UserAgent.UNKNOWN_VALUE;
+import static nl.basjes.parse.useragent.UserAgent.UNKNOWN_VERSION;
 
+import java.util.Objects;
 import nl.basjes.parse.useragent.UserAgent;
 import nl.basjes.parse.useragent.UserAgentAnalyzer;
 
@@ -22,10 +25,18 @@ public class UserAgentParserStrategyImpl implements UserAgentParserStrategy {
   public UserAgentParserResult parse(String userAgent) {
     UserAgent agent = analyzer.parse(userAgent);
 
-    String osName = agent.getValue(OPERATING_SYSTEM_NAME);
-    String osVersion = agent.getValue(OPERATING_SYSTEM_VERSION);
+    String tmp = agent.getValue(OPERATING_SYSTEM_NAME);
+
+    String osName = Objects.equals(tmp, UNKNOWN_VALUE) ? "unknown" : tmp;
+    tmp = agent.getValue(OPERATING_SYSTEM_VERSION);
+    String osVersion = Objects.equals(tmp, UNKNOWN_VERSION) ? "unknown" : tmp;
     String browserName = agent.getValue(AGENT_NAME);
     String browserVersion = agent.getValue(AGENT_VERSION);
+
+    if (Objects.equals(osName, browserName) && !Objects.equals(osName, "Hacker")) {
+      browserName = "unknown";
+      browserVersion = "unknown";
+    }
 
     OS os = new OS(osName, osVersion);
     Browser browser = new Browser(browserName, browserVersion);
